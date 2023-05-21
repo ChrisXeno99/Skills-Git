ea7f1e3bb2e2e3f7e452c42f6af186ad1df793b9
ChrisXeno99 <127868372+ChrisXeno99@users.noreply.github.com>
2023-05-21 19:43:27 +0100

    NewChanges

---
+++ Assets/Scripts/DinoAnimation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DinoAnimation : MonoBehaviour {
+
+	Animator MyAnimator;
+
+	void Start () {
+		MyAnimator = GetComponent<Animator> ();
+	}
+
+	void Update () {
+		if (Input.GetKey (KeyCode.RightArrow))
+		{
+			MyAnimator.SetBool ("walk", true);
+		}
+		else
+		{
+			MyAnimator.SetBool ("walk", false);
+		}
+			
+}
+}
