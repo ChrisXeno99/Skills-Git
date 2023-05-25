5f5bb499eec7baa0d6d3021efff8da6c765bdc96
ChrisXeno99 <127868372+ChrisXeno99@users.noreply.github.com>
2023-05-25 13:46:03 +0300

    task1 Working but not run or dead

--- Assets/Scripts/DinoAnimation.cs
+++ Assets/Scripts/DinoAnimation.cs
@@ -7,17 +7,43 @@ public class DinoAnimation : MonoBehaviour {
 	Animator MyAnimator;
 
 	void Start () {
-		MyAnimator = GetComponent<Animator> ();
+		MyAnimator = GetComponent<Animator>();
 	}
 
 	void Update () {
-		if (Input.GetKey (KeyCode.RightArrow))
+		if (Input.GetKey (KeyCode.RightArrow)) 
 		{
 			MyAnimator.SetBool ("walk", true);
+		} 
+		else if (Input.GetKey (KeyCode.DownArrow)) 
+		{
+			MyAnimator.SetBool ("dead", true);
+		} 
+		else if (Input.GetKey (KeyCode.R)) 
+		{
+			MyAnimator.SetBool ("run", true);
+		} 
+		else if (Input.GetKey (KeyCode.Space)) 
+		{
+			MyAnimator.Play("Jump");
+		} 
+		else if (MyAnimator.GetBool("dead") == false) 
+		{
+			MyAnimator.SetBool ("idle", true);
+			MyAnimator.SetBool ("walk", false);
+			MyAnimator.SetBool ("run", false);
+		} 
+		else if (Input.GetKey (KeyCode.Q) && MyAnimator.GetBool("dead")==true) 
+		{
+			MyAnimator.Play ("Respawn");
+			MyAnimator.SetBool ("dead", false);
 		}
 		else
 		{
-			MyAnimator.SetBool ("walk", false);
+			//MyAnimator.SetBool ("idle", true);
+			//MyAnimator.SetBool ("walk", false);
+			//MyAnimator.SetBool ("dead", false);
+			//MyAnimator.SetBool ("run", false);
 		}
 			
 }
