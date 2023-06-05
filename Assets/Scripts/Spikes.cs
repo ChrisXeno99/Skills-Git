0531c347cff77f2adec6b59e08ed3590cc097edd
ChrisXeno99 <127868372+ChrisXeno99@users.noreply.github.com>
2023-06-05 16:44:18 +0100

    Task2 completed
    Created platforms, made hero jump and lose on hitting spikes

---
+++ Assets/Scripts/Spikes.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spikes : MonoBehaviour {
+	GameObject Hero;
+	// Use this for initialization
+	void Start () {
+		Hero = GameObject.FindGameObjectWithTag ("Player");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnCollisionEnter2D (Collision2D other)
+	{
+		Debug.Log ("Spiked");
+		Hero.SendMessage("resetPosition");
+	}
+}
