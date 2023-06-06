d0484a8cfa8f58aca846afab0c097b3ab5e18670
ChrisXeno99 <127868372+ChrisXeno99@users.noreply.github.com>
2023-06-06 16:37:34 +0100

    Task 4 complete

--- Assets/Scripts/HeroMovement.cs
+++ Assets/Scripts/HeroMovement.cs
@@ -1,15 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class HeroMovement : MonoBehaviour {
 	Rigidbody2D rb;
 	float speed;
-		
+	public int lives;
+	bool vulnerable;
+
 	// Use this for initialization
 	void Start () {
 		speed = 5.1f;
 		rb = GetComponent<Rigidbody2D>();
+		lives = 3;
+		vulnerable = false;
 	}
 	
 	// Update is called once per frame
@@ -28,5 +33,35 @@ public class HeroMovement : MonoBehaviour {
 	{
 		Debug.Log ("SPIKED RECEIVED!");
 		transform.SetPositionAndRotation( new Vector3( -3.47f,2.9f,0),Quaternion.identity);
+		setLives();
+	}
+
+
+	public void setLives(){
+		lives -= 1;
+
+		if(lives <= 0 || vulnerable == true) 
+		{
+			Debug.Log("End of game");
+			SceneManager.LoadScene ("Lost");
+		}
+	}
+
+	public void OnCollisionEnter2D(Collision2D other){
+
+		if (other.gameObject.tag == "extraLife") {
+			lives += 1;
+			Destroy (other.gameObject);
+		} else if (other.gameObject.tag == "vulnerable") {
+			Destroy (other.gameObject);
+			vulnerable = true;
+			Debug.Log ("vulnerable = true!");
+			StartCoroutine("VulnerableDebuff");
+		}
+	}
+	IEnumerator VulnerableDebuff(){
+		yield return new WaitForSeconds (5f);
+		vulnerable = false;
+		Debug.Log("vulnerable = false!");
 	}
 }
